a06877fae220a51b7b73602c90b73f434c57362a
MertAliGozcu <mertaligozcu@gmail.com>
2020-11-09 00:02:28 +0300

    ThreadManager has been implemented

---
+++ PineconeGames.Core/PineconeGames.Core/Threads/PineconeThreadManager.cs
@@ -0,0 +1,39 @@
+﻿using PineconeGames.Core.Patterns;
+using System;
+
+namespace PineconeGames.Core.Threads
+{
+    #region Event Handlers
+
+    public delegate void ExecuteOnMainThreadEventHandler(Action action);
+
+    #endregion
+
+    public class PineconeThreadManager : Singleton<PineconeThreadManager>
+    {
+        #region Events
+
+        protected ExecuteOnMainThreadEventHandler _onExecuteOnMainThread;
+
+        #endregion
+
+        #region Public Functions
+
+        public void BindOnEvents(ExecuteOnMainThreadEventHandler onExecuteOnMainThread)
+        {
+            _onExecuteOnMainThread += onExecuteOnMainThread;
+        }
+
+        public void UnbindFromEvents(ExecuteOnMainThreadEventHandler onExecuteOnMainThread)
+        {
+            _onExecuteOnMainThread -= onExecuteOnMainThread;
+        }
+
+        public void ExecuteOnMainThread(Action action)
+        {
+            _onExecuteOnMainThread?.Invoke(action);
+        }
+
+        #endregion
+    }
+}
